b76adaa0f7388e3b1ee2af9437e47ebd6eb1411f
Marco De Martino <marco.demartino@student.supsi.ch>
2025-07-18 16:27:39 +0200

    helper per carousel

--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -2,24 +2,26 @@ import ProjectCard from "./ProjectCard";
 import "./ProjectCarousel.css";
 
 const ProjectCarousel = () => {
+  const img = (path: string) => `${import.meta.env.BASE_URL}${path}`;
+
   return (
     <div className="carousel">
       <ProjectCard
         typeItalic={"Interactive"}
         typeReg={"Exhibitions"}
-        images={["/images/angelucci.jpg"]}
+        images={[img("/images/angelucci.jpg")]}
         route={["projects/exmachina"]}></ProjectCard>
 
       <ProjectCard
         typeItalic={"Physical"}
         typeReg={"Interactions"}
-        images={["/images/nebulo.jpg", "images/rino.jpg"]}
+        images={[img("/images/nebulo.jpg"), img("images/rino.jpg")]}
         route={["projects/nebulo", "projects/rino"]}></ProjectCard>
 
       <ProjectCard
         typeItalic={"Digital"}
         typeReg={"Products"}
-        images={["/images/horizon.png", "/images/designdose.png"]}
+        images={[img("/images/horizon.png"), img("/images/designdose.png")]}
         route={["projects/horizon", "/projects/designdose"]}></ProjectCard>
     </div>
   );
