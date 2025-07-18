464ab7dc1147477baf2a3722b717ca43cd7d2d4b
Marco De Martino <marco.demartino@student.supsi.ch>
2025-07-18 03:06:47 +0200

    again

--- src/main.tsx
+++ src/main.tsx
@@ -58,7 +58,7 @@ const router = createBrowserRouter(
       element: <Contacts />,
     },
   ],
-  { basename: "/marco-portfolio" }
+  { basename: "/marco-portfolio/" }
 );
 
 createRoot(document.getElementById("root")!).render(
