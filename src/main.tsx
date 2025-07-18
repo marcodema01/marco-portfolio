aa1ec41d44bf885e10119063d12f73203a5aee9c
Marco De Martino <marco.demartino@student.supsi.ch>
2025-07-18 09:49:57 +0200

    actions

--- src/main.tsx
+++ src/main.tsx
@@ -13,53 +13,50 @@ import ProjectDescriptionDdose from "./pages/ProjectDescriptionDdose.tsx";
 import PlaygroundGrid from "./pages/PlaygroundGrid.tsx";
 import Contacts from "./pages/Contacts.tsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      // path: "/",
-      element: <App></App>,
-      children: [
-        {
-          path: "/",
-          element: <ProjectCarousel></ProjectCarousel>,
-        },
-        {
-          path: "projects/exmachina",
-          element: <ProjectDescription />,
-        },
-        {
-          path: "projects/horizon",
-          element: <ProjectDescriptionHorizon />,
-        },
-        {
-          path: "projects/nebulo",
-          element: <ProjectDescriptionNebulo />,
-        },
-        {
-          path: "projects/rino",
-          element: <ProjectDescriptionRino />,
-        },
-        {
-          path: "projects/designdose",
-          element: <ProjectDescriptionDdose />,
-        },
-        {
-          path: "playground",
-          element: <PlaygroundGrid />,
-        },
-      ],
-    },
-    {
-      path: "about",
-      element: <About />,
-    },
-    {
-      path: "contacts",
-      element: <Contacts />,
-    },
-  ],
-  { basename: "/marco-portfolio/" }
-);
+const router = createBrowserRouter([
+  {
+    // path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <ProjectCarousel></ProjectCarousel>,
+      },
+      {
+        path: "projects/exmachina",
+        element: <ProjectDescription />,
+      },
+      {
+        path: "projects/horizon",
+        element: <ProjectDescriptionHorizon />,
+      },
+      {
+        path: "projects/nebulo",
+        element: <ProjectDescriptionNebulo />,
+      },
+      {
+        path: "projects/rino",
+        element: <ProjectDescriptionRino />,
+      },
+      {
+        path: "projects/designdose",
+        element: <ProjectDescriptionDdose />,
+      },
+      {
+        path: "playground",
+        element: <PlaygroundGrid />,
+      },
+    ],
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "contacts",
+    element: <Contacts />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
